9db9e743b0a6da158b485cf752e402c2f3a6a3c9
Martina <161827443+MartiChiodo@users.noreply.github.com>
2024-11-27 17:02:09 +0100

    implementation of modified newton method

--- codes/main.m
+++ codes/main.m
@@ -4,10 +4,53 @@
 clear all
 clc
 
-format long e
 
 % Rosenbrock function in dimension n=2
 f= @(x) 100*(x(2)-x(1)^2)^2+(1-x(1))^2; 
 x0=[1.2;1.2];
 
-[xbest,xseq,iter,fbest, flag, failure] = nelderMead(f,x0,[],[],[],[],50,1e-9)
+
+function f = parametric_rosenbrock(x, alpha)
+    f = 0;
+    n = length(x);
+    for i = 1:(n-1)
+        f = f + alpha * (x(i+1) - x(i)^2)^2 + (1 - x(i))^2;
+    end
+end
+
+function gradf = grad_parametric_rosenbrock(x,alpha)
+    n = length(x);
+    gradf = zeros(n,1);
+    gradf(1,1) = -4*alpha*(x(2)-x(1)^2)*x(1) -4*(1-x(1)^2)*x(1);
+    gradf(n,1) = 2*alpha*(x(n)-x(n-1)^2);
+
+    for k = 2:n-1
+        gradf(k, 1) = -4*alpha*(x(k+1)-x(k)^2)*x(k) -4*(1-x(k)^2)*x(k) + 2*alpha*(x(k)-x(k-1)^2);
+    end
+end
+
+function Hessf = hess_parametric_rosenbrock(x,alpha)
+    n = length(x);
+    Hessf = zeros(n,n);
+
+    for i = 1:n-1
+        % Diagonal terms
+        Hessf(i, i) = Hessf(i, i) + 2 - 4 * alpha * (x(i+1) - x(i)^2) + 12 * alpha * x(i)^2;
+        Hessf(i+1, i+1) = Hessf(i+1, i+1) + 2 * alpha;
+        
+        % Off-diagonal terms
+        Hessf(i, i+1) = Hessf(i, i+1) - 4 * alpha * x(i);
+        Hessf(i+1, i) = Hessf(i+1, i) - 4 * alpha * x(i);
+    end
+
+end
+
+f = @(x) parametric_rosenbrock(x, 100);
+gradf = @(x) grad_parametric_rosenbrock(x,100);
+Hessf = @(x) hess_parametric_rosenbrock(x,100);
+
+[xbest_NM,xseq_NM,iter_NM,fbest_NM, flag, failure_NM] = nelderMead(f,x0,[],[],[],[],50,1e-9);
+x0 = 2 * ones(1000,1);
+
+itermax = 500; rho = 0.8; c1 = 1e-4; btmax = 50; tolgrad = 1e-9; tau_kmax = 10; 
+[xbest_MN, xseq_MN, iter_MN, fbest_MN, gradfk_norm_MN, btseq_MN, flag_bcktrck_MN, failure_MN] = modified_Newton(f,gradf, Hessf, x0, itermax, rho, c1, btmax, tolgrad, tau_kmax);
---
+++ codes/modified_Newton.m
@@ -0,0 +1,117 @@
+function [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf, Hessf, x0, itermax, rho, c1, btmax, tolgrad, tau_kmax)
+
+%%%% DESCRIZIONE INPUT/OUTPUT
+% flag indica se il backtracking ha funzionato 
+% tolgrad è usato come stopping criterion per il gradiente
+% tau_kmax max iter per determinare Bk
+
+% Function handle for the armijo condition
+farmijo = @(fk, alpha, c1_gradfk_pk) fk + alpha * c1_gradfk_pk;
+
+% initializing quantities
+n = length(x0); %dimension
+xseq = zeros(n,itermax + 1);
+xseq(:,1) = x0;
+btseq = zeros(itermax,1);
+failure = false;
+flag_bcktrck = false;
+
+fk = f(x0);
+gradfk = gradf(x0);
+Hessfk = Hessf(x0);
+k = 0;
+
+while k < itermax && sum(gradfk.^2) > tolgrad^2
+    
+    % computing Bk according the Algorith 3.3 of the book
+    beta = 1e-3;
+    min_diag = min(diag(Hessfk));
+
+    % initializing the tau
+    if min_diag > 0
+        tau_0 = 0;
+    else
+        tau_0 = -min_diag +beta;
+    end
+
+    for k_tau = 1:tau_kmax
+        % attempt to compute the Choleski factorization
+        failure_chol = false;
+        try
+            R = chol(Hessfk + tau_0 * eye(n));
+            
+        catch
+            failure_chol = true;
+        end
+
+        if failure_chol
+            tau = max(5*tau_0, beta);
+        else
+            % we already solve the linear system to compute pk exploiting
+            % the choleski factorization
+            y = -  R' \ gradfk;
+            pk = R \ y;
+            break
+        end
+    end
+
+    if k_tau == tau_kmax && failure_chol
+        disp("ALGORITMO 3.3 HA FALLITO")
+        xbest = 0; fbest = 0; iter = 0; gradfk_norm = 0;
+        return
+    end
+
+    % BACKTRACKING
+    % Reset the value of alpha
+    alpha = 1;
+    
+    % Compute the candidate new xk
+    xnew = x0 + alpha * pk;
+    % Compute the value of f in the candidate new xk
+    fnew = f(xnew);
+    
+    c1_gradfk_pk = c1 * gradfk' * pk;
+    bt = 0;
+    % Backtracking strategy: 
+    % 2nd condition is the Armijo condition not satisfied
+    while bt < btmax && fnew > farmijo(fk, alpha, c1_gradfk_pk)
+        % Reduce the value of alpha
+        alpha = rho * alpha;
+        % Update xnew and fnew w.r.t. the reduced alpha
+        xnew = x0 + alpha * pk;
+        fnew = f(xnew);
+        
+        % Increase the counter by one
+        bt = bt + 1;
+    end
+    if bt == btmax && fnew > farmijo(fk, alpha, c1_gradfk_pk)
+        flag_bcktrck = true;
+        break
+    end
+
+    x0 = xnew;
+
+
+    % preparing for the next iteration
+    k = k+1;
+    fk = f(x0);
+    xseq(:,k+1) = x0;
+    btseq(k,1) = bt;
+    gradfk = gradf(x0);
+    Hessfk = Hessf(x0);
+
+end
+
+% declaring failure if this is the case
+if k == itermax && sum(gradfk.^2) > tolgrad^2
+    failure = true;
+end
+
+xbest = x0;
+fbest = fk;
+iter = k;
+xseq = xseq(:, 1:iter+1);
+btseq = btseq(1:iter,1);
+gradfk_norm = norm(gradfk);
+
+end
