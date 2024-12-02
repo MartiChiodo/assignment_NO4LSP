807467178ef542b7f2578a37ef9b462fb1ef4944
Martina <161827443+MartiChiodo@users.noreply.github.com>
2024-12-02 10:04:57 +0100

    ho tolto un file che era finito lì per sbaglio

--- codes/es3_marti.m
+++ codes/es3_marti.m
@@ -22,22 +22,26 @@ end
 
 f = @(x) PF1_funct(x);
 gradf = @(x) x .* (2+1e-5) -1e-5;
-Hessf = @(x) (2+1e-5)*eye(length(x));
+Hessf = @(x) spdiags((2+1e-5)*ones(length(x),1), 0, length(x), length(x));
 
 
 % setting the values for the dimension
-dimension = [1e2 1e3 1e4];
+dimension = [1e2 1e3];
 iter_max = 100;
-tol = 1e-9;
-avg_execution_time_SX = zeros(3,1);
-failure_struct_SX = zeros(3,1); %for each dimension we count the number of failure
+tol = 1e-7;
+avg_execution_time_SX = zeros(length(dimension),1);
+failure_struct_SX = zeros(length(dimension),1); %for each dimension we count the number of failure
+avg_execution_time_MN = zeros(length(dimension),1);
+failure_struct_MN = zeros(length(dimension),1); %for each dimension we count the number of failure
+
 
  % initializing structures
-iter_struct_SX = zeros(1,3);
-fbest_struct_SX = zeros(1,3);
+iter_struct_SX = zeros(length(dimension),1);
+fbest_struct_SX = zeros(length(dimension),1);
+iter_struct_MN = zeros(length(dimension),1);
+fbest_struct_MN = zeros(length(dimension),1);
 
-
-for dim = 1:3
+for dim = 1:length(dimension)
     n = dimension(dim);
     x0 = (1:1:n)';
     
@@ -45,55 +49,88 @@ for dim = 1:3
     x0_rndgenerated = zeros(n,10);
     x0_rndgenerated(1:n, :) = x0(1:n) - 1 + 2.*rand(n,10);
     
-    % conditions for backtracking (poi le sistemo quando implementiamo il secondo metodo)
-    ro = 0.5;
-    c = 1e-4;
 
     %
-    f_best_avg = 0;
-    x_best_avg = zeros(n,1);
-    iter_avg = 0;
+    f_best_avg_SX = 0;
+    iter_avg_SX = 0;
+    f_best_avg_MN = 0;
+    iter_avg_MN = 0;
 
     % I can measure the computanional averge time
-    time = 0;
+    time_SX = 0;
+    time_MN = 0;
 
+    % SOLVING SIMPLEX METHOD
     % first initial point
-    fprintf('solving the NM method for the first x0 with dim = %i \n', n)
+    fprintf('solving the SX method for the first x0 with dim = %i \n', n)
     t1 = tic;
-    [xbest, ~,iter,fbest, flag, failure] = nelderMead(f,x0,[],[],[],[],iter_max*size(x0,1),tol);
-    time = time + toc(t1);
-    f_best_avg = f_best_avg + fbest;
-    iter_avg = iter_avg + iter;
+    [~, ~,iter,fbest, ~, failure] = nelderMead(f,x0,[],[],[],[],iter_max*size(x0,1),tol);
+    time_SX = time_SX + toc(t1);
+    f_best_avg_SX = f_best_avg_SX + fbest;
+    iter_avg_SX = iter_avg_SX + iter;
 
 
     % if failure = true (failure == 1), the run was unsuccessful; otherwise
     % failure = 0
-    failure_struct_SX(dim) = failure_struct_SX(dim) + failure;
+    failure_struct_SX(dim,1) = failure_struct_SX(dim,1) + failure;
 
     for i = 1:10
-        fprintf('solving the NM method for the %i -th x0 with dim = %i \n', i+1, n)
+        fprintf('solving the SX method for the %i -th x0 with dim = %i \n', i+1, n)
         t1 = tic;
-        [xbest,~,iter,fbest, flag, failure] = nelderMead(f,x0_rndgenerated(:,i),[],[],[],[],iter_max*size(x0,1),tol);
-        time = time + toc(t1);
-        f_best_avg = f_best_avg + fbest;
-        x_best_avg = x_best_avg + xbest;
-        iter_avg = iter_avg + iter;
-
-        % if failure = true (failure == 1), the run was unsuccessful; otherwise
-        % failure = 0
-        failure_struct_SX(dim) = failure_struct_SX(dim) + failure;
+        [~,~,iter,fbest, ~, failure] = nelderMead(f,x0_rndgenerated(:,i),[],[],[],[],iter_max*size(x0,1),tol);
+        time_SX = time_SX + toc(t1);
+        f_best_avg_SX = f_best_avg_SX + fbest;
+        iter_avg_SX = iter_avg_SX + iter;
+        failure_struct_SX(dim,1) = failure_struct_SX(dim,1) + failure;
     end
 
-    fbest_struct_SX(dim) = f_best_avg/11;
-    iter_struct_SX(1,dim) = iter_avg/11;
+    fbest_struct_SX(dim,1) = f_best_avg_SX/11;
+    iter_struct_SX(dim,1) = iter_avg_SX/11;
 
     % I store the average execution time
-    avg_execution_time_SX(dim,1) = time/11;
+    avg_execution_time_SX(dim,1) = time_SX/11;
+
+
+    % SOLVING MODIFIED NEWTON METHOD METHOD
+    % first initial point
+    rho = 0.5; c1 = 1e-4; btmax = 50; tau_kmax = 1e4; tol = 1e-7;
+    fprintf('solving the MN method for the first x0 with dim = %i \n', n)
+    t1 = tic;
+    [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf, Hessf, x0, iter_max, rho, c1, btmax, tol, tau_kmax);       
+    time_MN = time_MN + toc(t1);
+    f_best_avg_MN = f_best_avg_MN + fbest;
+    iter_avg_MN = iter_avg_MN + iter;
+
+
+    % if failure = true (failure == 1), the run was unsuccessful; otherwise
+    % failure = 0
+    failure_struct_MN(dim,1) = failure_struct_MN(dim,1) + failure;
+
+    for i = 1:10
+        fprintf('solving the MN method for the %i -th x0 with dim = %i \n', i+1, n)
+        t1 = tic;
+        [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf, Hessf, x0, iter_max, rho, c1, btmax, tol, tau_kmax);       
+        time_MN = time_MN + toc(t1);
+        f_best_avg_MN = f_best_avg_MN + fbest;
+        iter_avg_MN = iter_avg_MN + iter;
+        failure_struct_MN(dim,1) = failure_struct_MN(dim,1) + failure;
+    end
+
+    fbest_struct_MN(dim,1) = f_best_avg_MN/11;
+    iter_struct_MN(dim,1) = iter_avg_MN/11;
+
+    % I store the average execution time
+    avg_execution_time_MN(dim,1) = time_MN/11;
 
 end
 
-failure_struct_SX
-avg_execution_time_SX
-fbest_struct_SX
-iter_struct_SX
+
+TSX = table( dimension', fbest_struct_SX, iter_struct_SX, avg_execution_time_SX, failure_struct_SX);
+format bank
+display(TSX)
+
+TMN = table( dimension', fbest_struct_MN, iter_struct_MN, avg_execution_time_MN, failure_struct_MN);
+format bank
+display(TMN)
+
 
