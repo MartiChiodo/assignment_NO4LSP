7e2e75e216b3201556f8bcdb3196f81fc359ecdc
Martina <161827443+MartiChiodo@users.noreply.github.com>
2024-12-07 19:09:35 +0100

    some fix on the HEssians

--- codes/PROVA.m
+++ codes/PROVA.m
@@ -8,20 +8,29 @@ function Fx = PF1_funct(x)
     % Fx is a vector, the i-th element is F(x(:,i))
 
     Fx = zeros(1,size(x,2));
+    size(x,1)
     for col = 1:size(x,2)
         Fx(1,col) = 0.5* 1e-5 * sum((x(:,col) - ones(size(x,1),1)).^2) + 0.5*(sum(x(:,col).^2) - 0.25)^2;
     end
 end
 
 f = @(x) PF1_funct(x);
-gradf = @(x) x .* (2+1e-5) -1e-5;
-Hessf = @(x) spdiags((2+1e-5)*ones(length(x),1), 0, length(x), length(x));
+gradf = @(x) x .* (2+1e-5) -1e-5 + (sum(x.^2) -0.25).*x;
 
+function hessf = hessian(x)
+    n = length(x);
+    diags = zeros(n,3); %1st column is the principal diag, 2nd column is the superior diag and 3rd column is the inferior
+    diags(1:n,1) = 1e-5 + 4*x(1:n).^2 + 2*(sum(x(:,1).^2) -0.25);
+    diags(2:n,3) = 4.*x(1:n-1).*x(2:n);
+    diags(1:n-1,2) = 4.*x(2:n).*x(1:n-1);
+    hessf = spdiags(diags, [0,1,-1], n,n);
+end
 
+Hessf = @(x) hessian(x);
 
 %% vediamo se il minimo varia all'aumentare della dimensione o se è costante
-x = -5:0.1:5;
-y = -5:0.1:5;
+x = -15:0.1:15;
+y = -15:0.1:15;
 [X, Y] = meshgrid(x, y);
 Z = arrayfun(@(x, y) f([x; y]), X, Y);
 
@@ -34,7 +43,12 @@ ylabel('y');
 zlabel('f(x, y)');
 shading interp; 
 
-
+% % curve di livello
+% contour(X, Y, Z, 20); % '20' indica il numero di livelli
+% colorbar; % Aggiunge una barra colori per riferimento
+% xlabel('x');
+% ylabel('y');
+% title('Curve di livello della funzione f');
 
 
 %% prova NEALDER MEAD MATLAB
@@ -46,16 +60,21 @@ x0
 
 %% PROVA MODIFIED NEWTON METHOD
 tol = 1e-7;
-n = 1e5;
-x0 = (1:1:n)';
-rho = 0.5; c1 = 1e-4; btmax = 50; tau_kmax = 1e4; 
+n = 1e2;
+% x0 = (1:1:n)'; % pto iniziale es3_marti
+x0 = ones(n,1); % pto iniziale Rosenbrock
+x0(1:2:n) = -1.2;
+
+rho = 0.5; c1 = 1e-4; btmax = 45; tau_kmax = 1e4; 
 [xbest_MN, xseq_MN, iter_MN, fbest_MN, gradfk_norm_MN, btseq_MN, flag_bcktrck_MN, failure_MN] = modified_Newton(f,gradf, Hessf, x0, 1000, rho, c1, btmax, tol, tau_kmax);
 
 
-%% NEALDER MEAD
-n = 1e2;
-x0 = (1:1:n)';
-[xbest, xseq,iter,fbest, flag, failure] = nelderMead(f,x0,[],[],[],[],n*500,[]);
-iter
-failure
-xbest
+%% PROVANEALDER MEAD
+n = 50;
+% x0 = (1:1:n)'; % pto iniziale es3_marti
+x0 = ones(n,1); % pto iniziale Rosenbrock
+x0(1:2:n) = -1.2;
+
+[xbest, xseq,iter,fbest, flag, failure] = nelderMead(f,x0,[],[],[],[],n*500,[])
+
+
--- codes/main.m
+++ codes/main.m
@@ -44,18 +44,17 @@ function Hessf = hess_parametric_rosenbrock(x,alpha)
 
     diags(1,1) = 2 + 8*alpha*x(1) - 4*alpha*x(2);
     diags(n,1) = 2*alpha;
-    diags(2,3) = -4*alpha*x(1);
-    diags(n-1,2) = -4*alpha*x(n-1);
-
+    diags(n-1,3) = -4*alpha*x(n-1);
+    diags(n,2) = -4*alpha*x(n-1);
 
     for k = 2:n-1
        diags(k,1) = 2*alpha*8*alpha*x(k) - 4*alpha*x(k+1) +2;
-       diags(k+1,3) = -4*alpha*x(k+1); %diag inferior: k is the first derivative
-       diags(k-1,2)= -4*alpha*x(k); %diag superior: k id the first derivative
+       diags(k-1,3) = -4*alpha*x(k-1); %diag inferior: k is the first derivative
+       diags(k,2)= -4*alpha*x(k-1); %diag superior: k id the first derivative
     end
     
 
-    Hessf = spdiags(diags, [0, 1, -1], n, n);
+    Hessf = spdiags(diags, [0, +1, -1], n, n);
 
 end
 
@@ -64,7 +63,7 @@ f = @(x) parametric_rosenbrock(x, 100);
 gradf = @(x) grad_parametric_rosenbrock(x,100);
 Hessf = @(x) hess_parametric_rosenbrock(x,100);
 
-Hessf([1,1])
+%%
 
 % initial points for the algorithms
 x0_a = [1.2; 1.2];
@@ -72,7 +71,7 @@ x0_b = [-1.2; 1];
 x_esatto = [1;1];
 
 tol = 1e-8;
-rho = 0.5; c1 = 1e-4; btmax = 50; tau_kmax = 10; 
+rho = 0.5; c1 = 1e-1; btmax = 45; tau_kmax = 100; 
 
 time_SX = 0;
 time_MN = 0;
@@ -83,7 +82,7 @@ t1 = tic;
 [xbest_SX_a,xseq_SX_a,iter_SX_a,fbest_SX_a, flag_SX_a, failure_SX_a] = nelderMead(f,x0_a,[],[],[],[],400,tol);
 time_SX = time_SX + toc(t1);
 t1 = tic;
-[xbest_MN_a, xseq_MN_a, iter_MN_a, fbest_MN_a, gradfk_norm_MN_a, btseq_MN_a, flag_acktrck_MN_a, failure_MN_a] = modified_Newton(f,gradf, Hessf, x0_a, 1000, rho, c1, btmax, tol, tau_kmax);
+[xbest_MN_a, xseq_MN_a, iter_MN_a, fbest_MN_a, gradfk_norm_MN_a, btseq_MN_a, flag_bcktrck_MN_a, failure_MN_a] = modified_Newton(f,gradf, Hessf, x0_a, 1000, rho, c1, btmax, tol, tau_kmax);
 time_MN = time_MN + toc(t1);
 t1 = tic;
 [xbest_SX_b,xseq_SX_b,iter_SX_b,fbest_SX_b, flag_SX_b, failure_SX_b] = nelderMead(f,x0_b,[],[],[],[],400,tol);
@@ -97,9 +96,9 @@ failure = [failure_SX_a + failure_SX_b;  failure_MN_a + failure_MN_b];
 avg_iter = [(iter_SX_b+iter_SX_a)/2; (iter_MN_b + iter_MN_a)/2];
 avg_time_execution = [time_SX/2; time_MN/2];
 roc = [(compute_roc(x_esatto, xseq_SX_b)+ compute_roc(x_esatto, xseq_SX_a))/2; (compute_roc(x_esatto, xseq_MN_b)+ compute_roc(x_esatto, xseq_MN_a))/2];
+avg_gradfk = [NaN; (gradfk_norm_MN_a +gradfk_norm_MN_b)/2];
 
-
-T = table( failure, avg_iter, avg_time_execution, roc, 'RowNames', {'simplex method'; 'modified Newton'});
+T = table( failure, avg_gradfk, avg_iter, avg_time_execution, roc, 'RowNames', {'simplex method'; 'modified Newton'});
 format bank
 display(T)
 
