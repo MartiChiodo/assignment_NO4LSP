ef7d979b1784a1de3d60a86f291ce3929be977ee
Martina <161827443+MartiChiodo@users.noreply.github.com>
2025-01-02 17:49:30 +0100

    implementazione differenze finite pb 76 e pb 64

--- codes/problem_76.m
+++ codes/problem_76.m
@@ -84,7 +84,6 @@ format short e
 
 % setting the dimensionality
 dimension = [10 25 50];
-rng(seed);
 
 
 % initializing the structures to store some stats
@@ -101,6 +100,7 @@ for dim = 1:length(dimension)
     x0 = 2*ones(n,1);
 
     % in order to generate random number in [a,b] I apply the formula r = a + (b-a).*rand(n,1)
+    rng(seed);
     x0_rndgenerated = zeros(n,10);
     x0_rndgenerated(1:n, :) = x0(1:n) - 1 + 2.*rand(n,10);
 
@@ -198,7 +198,6 @@ dimension = [1e3 1e4 1e5];
 
 param = [0.4, 1e-4, 40; 0.3, 1e-4, 28; 0.4, 1e-3, 36];
 
-rng(seed);
 
 % initializing structures to store some stats
 execution_time_MN = zeros(length(dimension),11);
@@ -218,6 +217,7 @@ for dim = 1:length(dimension)
     x0 = 2*ones(n,1);
     
     % in order to generate random number in [a,b] I apply the formula r = a + (b-a).*rand(n,1)
+    rng(seed);
     x0_rndgenerated = zeros(n,10);
     x0_rndgenerated(1:n, :) = x0(1:n) - 1 + 2.*rand(n,10);
     
@@ -313,34 +313,53 @@ display(TMN)
 %% FINITE DIFFERENCES
 clc
 
-function grad_approx = findiff_grad_76(x, h)
+function grad_approx = findiff_grad_76(x, h, type_h)
+    %   - type_h: indica se la derivata è calcolata con h costante o h
+    %   relativo
     n = length(x);
-    if isscalar(h)
-        passo = h;
+
+    if isempty(type_h)
+        type_h = 'COST';
     end
+
+    switch type_h
+        case 'COST'
+            passo1 = h;
+            passok = h;
+            passon = h;
+        case 'REL'
+            passo1 = h*abs(x(1));
+            passon = h*abs(x(n));
+    end
+
     grad_approx = zeros(n,1);
-    grad_approx(1,1) =  passo*(4*x(1)  - 2/5 * x(2)^2 + (8*x(1)*(x(1)^2+passo^2))/100 - 4/5 * x(n)*x(1));
+    grad_approx(1,1) =  passo1*(4*x(1)  - 2/5 * x(2)^2 + (8*x(1)*(x(1)^2+passo1^2))/100 - 4/5 * x(n)*x(1))/(4*passo1);
     
     for k = 2:n-1
-        grad_approx(k,1) = passo*(4*x(k)  - 2/5 * x(k+1)^2 + (8*x(k)*(x(k)^2+passo^2))/100 - 4/5 * x(k-1)*x(k));
+        if strcmp('REL', type_h)
+            passok = h*abs(x(k));
+        end
+        grad_approx(k,1) = passok*(4*x(k)  - 2/5 * x(k+1)^2 + (8*x(k)*(x(k)^2+passok^2))/100 - 4/5 * x(k-1)*x(k))/(4*passok);
     end
-    grad_approx(n,1) = passo*(4*x(n)  - 2/5 * x(1)^2 + (8*x(n)*(x(n)^2+passo^2))/100 - 4/5 * x(n-1)*x(n));
+    grad_approx(n,1) = passon*(4*x(n)  - 2/5 * x(1)^2 + (8*x(n)*(x(n)^2+passon^2))/100 - 4/5 * x(n-1)*x(n))/(4*passon);
     
-    grad_approx = grad_approx./(4*passo);
 end
 
 
-function hessian_approx = findiff_hess_76(x, h)
+function hessian_approx = findiff_hess_76(x, h, type_h)
     % Calcola la matrice Hessiana sparsa per la funzione f(x)
     % Input:
     %   - x: vettore colonna (punto in cui calcolare l'Hessiana)
     %   - h: passo o vettore per la differenzaz rispetto ad una componente
+    %   - type_h: indica se la derivata è calcolata con h costante o h
+    %   relativo
     % Output:
     %   - H: matrice Hessiana sparsa
 
     n = length(x); % Dimensione del problema
-    if isscalar(h)
-        passo = h;
+
+    if isempty(type_h)
+        type_h = 'COST';
     end
     
     % Preallocazione per la struttura sparsa
@@ -353,22 +372,40 @@ function hessian_approx = findiff_hess_76(x, h)
     for k = 1:n
         % Elementi diagonali H(k, k)
         if k == 1
+            switch type_h
+                case 'REL'
+                    passok = h*abs(x(k));
+                case 'COST'
+                    passok = h;
+            end
             % H_kk = (fn_quadro(x+2*he_k) + fk_quadro(x+2*he_k, 1) - 2*fn_quadro(x+he_k) -2*fk_quadro(x+he_k,1) + fn_quadro(x) + fk_quadro(x,1))/(2*h^2);
-            H_kk = (2*passo^2 - 2/5 * x(n)*passo^2+ 0.12 * x(k)^2*passo^2+ 0.24 * x(k)*passo^3 +0.14 * passo^4)/(2*passo^2); 
+            H_kk = (2*passok^2 - 2/5 * x(n)*passok^2+ 0.12 * x(k)^2*passok^2+ 0.24 * x(k)*passok^3 +0.14 * passok^4)/(2*passok^2); 
             i_indices(cont) = k;
             j_indices(cont) = k;
             values(cont) = H_kk;
             cont = cont +1;
         elseif k < n
+            switch type_h
+                case 'REL'
+                    passok = h*abs(x(k));
+                case 'COST'
+                    passok = h;
+            end
             % H_kk = (fk_quadro(x+2*he_k, k-1) + fk_quadro(x+2*he_k, k) -  2*fk_quadro(x+he_k, k-1) -2*fk_quadro(x+he_k,k) + fk_quadro(x,k-1) + fk_quadro(x,k))/(2*h^2);
-            H_kk = (2*passo^2 - 2/5 * x(k-1)*passo^2+ 0.12 * x(k)^2*passo^2+ 0.24 * x(k)*passo^3 +0.14 * passo^4)/(2*passo^2); 
+            H_kk = (2*passok^2 - 2/5 * x(k-1)*passok^2+ 0.12 * x(k)^2*passok^2+ 0.24 * x(k)*passok^3 +0.14 * passok^4)/(2*passok^2); 
             i_indices(cont) = k;
             j_indices(cont) = k;
             values(cont) = H_kk;
             cont = cont +1;
         else
+            switch type_h
+                case 'REL'
+                    passok = h*abs(x(n));
+                case 'COST'
+                    passok = h;
+            end
             % H_kk = (fk_quadro(x+2*he_k, k-1) + fn_quadro(x+2*he_k) -  2*fk_quadro(x+he_k, k-1) -2*fn_quadro(x+he_k) + fk_quadro(x,k-1) + fn_quadro(x))/(2*h^2);
-            H_kk = (2*passo^2 - 2/5 * x(n-1)*passo^2+ 0.12 * x(k)^2*passo^2+ 0.24 * x(k)*passo^3 +0.14 * passo^4)/(2*passo^2); 
+            H_kk = (2*passok^2 - 2/5 * x(n-1)*passok^2+ 0.12 * x(k)^2*passok^2+ 0.24 * x(k)*passok^3 +0.14 * passok^4)/(2*passok^2); 
             i_indices(cont) = k;
             j_indices(cont) = k;
             values(cont) = H_kk;
@@ -378,7 +415,7 @@ function hessian_approx = findiff_hess_76(x, h)
         % Elementi fuori diagonale H(k, k+1)
         if k < n
             % H_k_k1 = (fk_quadro(x+he_k1 +he_k,k) - fk_quadro(x+he_k, k) - fk_quadro(x+he_k1,k) - fk_quadro(x, k))/(2*h^2); 
-            H_k_k1 = (-2/5 *passo^2*x(k+1) - 1/5 * passo^3)/(2*passo^2);
+            H_k_k1 = (-2/5 *passok^2*x(k+1) - 1/5 * passok^3)/(2*passok^2);
             i_indices(cont) = k;
             j_indices(cont) = k+1;
             values(cont) = H_k_k1;
@@ -391,7 +428,7 @@ function hessian_approx = findiff_hess_76(x, h)
             cont = cont+1;
         else
             % Caso circolare: H(n, 1)
-            H_n1 = (-2/5 *passo^2*x(1) - 1/5 * passo^3)/(2*passo^2);
+            H_n1 = (-2/5 *passok^2*x(1) - 1/5 * passok^3)/(2*passok^2);
             i_indices(cont) = 1;
             j_indices(cont) = n;
             values(cont) = H_n1;
@@ -410,108 +447,74 @@ function hessian_approx = findiff_hess_76(x, h)
     hessian_approx = sparse(i_indices, j_indices, values, n, n);
 end
 
-h = 1e-4;
-gradf_approx = @(x) findiff_grad_76(x,h);
-hessf_approx = @(x) findiff_hess_76(x,h);
-
-n=15;
-
-vec = [2; 0.3; 0.3; 2; 0.9; 1; 0.2];
-gradf(vec)
-gradf_approx(vec)
-
-full(Hessf(vec))
-full(hessf_approx(vec))
-
 
 
 %% RUNNING THE EXPERIMENTS ON MODIFIED NEWTON METHOD WITH FIN DIFF
 format short e
+clc
 
 iter_max = 5000;
 tol = 1e-3;
 
+
+
 % setting the values for the dimension
+h_values = [1e-2 1e-4 1e-6 1e-8 1e-10 1e-12];
 dimension = [1e3 1e4 1e5];
-
 param = [0.4, 1e-4, 40; 0.3, 1e-4, 28; 0.4, 1e-3, 36];
-
-rng(seed);
+type_h = 'COST';
 
 % initializing structures to store some stats
-execution_time_MN = zeros(length(dimension),11);
-failure_struct_MN = zeros(length(dimension),11); %for each dimension we count the number of failure
-iter_struct_MN = zeros(length(dimension),11);
-fbest_struct_MN = zeros(length(dimension),11);
-gradf_struct_MN = zeros(length(dimension),11);
-roc_struct_MN = zeros(length(dimension),11);
-
-for dim = 1:length(dimension)
-    n = dimension(dim);
-
-    [rho, c1, btmax] = deal(param(dim, 1), param(dim, 2), param(dim, 3));
+execution_time_MN_h = zeros(length(dimension),6);
+failure_struct_MN_h = zeros(length(dimension),6); 
+iter_struct_MN_h = zeros(length(dimension),6);
+fbest_struct_MN_h = zeros(length(dimension),6);
+gradf_struct_MN_h = zeros(length(dimension),6);
+roc_struct_MN_h = zeros(length(dimension),6);
 
 
-    %defining the given initial point
-    x0 = 2*ones(n,1);
+for id_h = 1:length(h_values)
+
+   h = h_values(id_h);
+
+    gradf_approx = @(x) findiff_grad_76(x,h, type_h);
+    hessf_approx = @(x) findiff_hess_76(x,h, type_h);
+
+    % initializing structures to store some stats
+    execution_time_MN = zeros(length(dimension),11);
+    failure_struct_MN = zeros(length(dimension),11); %for each dimension we count the number of failure
+    iter_struct_MN = zeros(length(dimension),11);
+    fbest_struct_MN = zeros(length(dimension),11);
+    gradf_struct_MN = zeros(length(dimension),11);
+    roc_struct_MN = zeros(length(dimension),11);
     
-    % in order to generate random number in [a,b] I apply the formula r = a + (b-a).*rand(n,1)
-    x0_rndgenerated = zeros(n,10);
-    x0_rndgenerated(1:n, :) = x0(1:n) - 1 + 2.*rand(n,10);
     
-
-    % SOLVING MODIFIED NEWTON METHOD METHOD
-    % first initial point
-    t1 = tic;
-    [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf_approx, hessf_approx, x0, iter_max, rho, c1, btmax, tol, [], 'ALG', 0);       
-    execution_time_MN(dim,1) = toc(t1);
-    fbest_struct_MN(dim,1) = fbest;
-    iter_struct_MN(dim,1) = iter;
-    gradf_struct_MN(dim,1) = gradfk_norm;
-    roc_struct_MN(dim,1) = compute_roc(xseq);
-    disp(['**** MODIFIED NEWTON METHOD WITH FIN DIFF FOR THE PB 76 (point ', num2str(1), ', dimension ', num2str(n), '):  *****']);
-
-    disp(['Time: ', num2str(execution_time_MN(dim,1)), ' seconds']);
-    disp(['Backtracking parameters (rho, c1): ', num2str(rho), ' ', num2str(c1)]);
-
-    disp('**** MODIFIED NEWTON METHOD : RESULTS *****')
-    disp('************************************')
-    disp(['f(xk): ', num2str(fbest)])
-    disp(['norma di gradf(xk): ', num2str(gradfk_norm)])
-    disp(['N. of Iterations: ', num2str(iter),'/',num2str(iter_max)])
-    disp(['Rate of Convergence: ', num2str(roc_struct_MN(dim,1))])
-    disp('************************************')
-
-    if (failure)
-        disp('FAIL')
-        if (flag_bcktrck)
-            disp('Failure due to backtracking')
-        else
-            disp('Failure not due to backtracking')
-        end
-        disp('************************************')
-    else
-        disp('SUCCESS')
-        disp('************************************')
-    end
-    disp(' ')
-
-    % if failure = true (failure == 1), the run was unsuccessful; otherwise
-    % failure = 0
-    failure_struct_MN(dim,1) = failure_struct_MN(dim,1) + failure ;
-
-    for i = 1:10
+    for dim = 1:length(dimension)
+        n = dimension(dim);
+    
+        [rho, c1, btmax] = deal(param(dim, 1), param(dim, 2), param(dim, 3));
+    
+    
+        %defining the given initial point
+        x0 = 2*ones(n,1);
+        
+        % in order to generate random number in [a,b] I apply the formula r = a + (b-a).*rand(n,1)
+        rng(seed);
+        x0_rndgenerated = zeros(n,10);
+        x0_rndgenerated(1:n, :) = x0(1:n) - 1 + 2.*rand(n,10);
+        
+    
+        % SOLVING MODIFIED NEWTON METHOD METHOD
+        % first initial point
         t1 = tic;
         [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf_approx, hessf_approx, x0, iter_max, rho, c1, btmax, tol, [], 'ALG', 0);       
-        execution_time_MN(dim,i+1) = toc(t1);
-        fbest_struct_MN(dim,i+1) = fbest;
-        iter_struct_MN(dim,i+1) = iter;
-        failure_struct_MN(dim,i+1) = failure_struct_MN(dim,i+1) + failure;
-        gradf_struct_MN(dim,i+1) = gradfk_norm;
-        roc_struct_MN(dim,i+1) = compute_roc(xseq);
-
-        disp(['**** MODIFIED NEWTON METHOD WITH FIN DIFF FOR THE PB 76 (point ', num2str(i+1), ', dimension ', num2str(n), '):  *****']);
-
+        execution_time_MN(dim,1) = toc(t1);
+        fbest_struct_MN(dim,1) = fbest;
+        iter_struct_MN(dim,1) = iter;
+        gradf_struct_MN(dim,1) = gradfk_norm;
+        roc_struct_MN(dim,1) = compute_roc(xseq);
+        disp(['**** MODIFIED NEWTON METHOD WITH FIN DIFF ( ', type_h, ' with h = ', num2str(h), ') FOR THE PB 76 (point ', num2str(1), ', dimension ', num2str(n), '):  *****']);
+    
         disp(['Time: ', num2str(execution_time_MN(dim,1)), ' seconds']);
         disp(['Backtracking parameters (rho, c1): ', num2str(rho), ' ', num2str(c1)]);
     
@@ -536,13 +539,64 @@ for dim = 1:length(dimension)
             disp('************************************')
         end
         disp(' ')
+    
+        % if failure = true (failure == 1), the run was unsuccessful; otherwise
+        % failure = 0
+        failure_struct_MN(dim,1) = failure_struct_MN(dim,1) + failure ;
+    
+        for i = 1:10
+            t1 = tic;
+            [xbest, xseq, iter, fbest, gradfk_norm, btseq, flag_bcktrck, failure] = modified_Newton(f,gradf_approx, hessf_approx, x0, iter_max, rho, c1, btmax, tol, [], 'ALG', 0);       
+            execution_time_MN(dim,i+1) = toc(t1);
+            fbest_struct_MN(dim,i+1) = fbest;
+            iter_struct_MN(dim,i+1) = iter;
+            failure_struct_MN(dim,i+1) = failure_struct_MN(dim,i+1) + failure;
+            gradf_struct_MN(dim,i+1) = gradfk_norm;
+            roc_struct_MN(dim,i+1) = compute_roc(xseq);
+    
+            disp(['**** MODIFIED NEWTON METHOD WITH FIN DIFF ( ', type_h, ' with h = ', num2str(h), ') FOR THE PB 76 (point ', num2str(i+1), ', dimension ', num2str(n), '):  *****']);
+    
+            disp(['Time: ', num2str(execution_time_MN(dim,1)), ' seconds']);
+            disp(['Backtracking parameters (rho, c1): ', num2str(rho), ' ', num2str(c1)]);
+        
+            disp('**** MODIFIED NEWTON METHOD : RESULTS *****')
+            disp('************************************')
+            disp(['f(xk): ', num2str(fbest)])
+            disp(['norma di gradf(xk): ', num2str(gradfk_norm)])
+            disp(['N. of Iterations: ', num2str(iter),'/',num2str(iter_max)])
+            disp(['Rate of Convergence: ', num2str(roc_struct_MN(dim,1))])
+            disp('************************************')
+        
+            if (failure)
+                disp('FAIL')
+                if (flag_bcktrck)
+                    disp('Failure due to backtracking')
+                else
+                    disp('Failure not due to backtracking')
+                end
+                disp('************************************')
+            else
+                disp('SUCCESS')
+                disp('************************************')
+            end
+            disp(' ')
+        end
     end
+    
+    
+    
+    varNames = ["avg fbest", "avg gradf_norm","avg num of iters", "avg time of exec (sec)", "n failure", "avg roc"];
+    rowNames = string(dimension');
+    TMN = table(sum(fbest_struct_MN,2)/11, sum(gradf_struct_MN,2)/11 ,sum(iter_struct_MN,2)/11, sum(execution_time_MN,2)/11, sum(failure_struct_MN,2), sum(roc_struct_MN,2)/11,'VariableNames', varNames, 'RowNames', rowNames);
+    format bank
+    display(TMN)
+
+    execution_time_MN_h(:,id_h) = sum(execution_time_MN,2)/11;
+    failure_struct_MN_h(:,id_h) = sum(failure_struct_MN,2); 
+    iter_struct_MN_h(:,id_h) = sum(iter_struct_MN,2)/11;
+    fbest_struct_MN_h(:,id_h) = sum(fbest_struct_MN,2)/11;
+    gradf_struct_MN_h(:,id_h) = sum(gradf_struct_MN,2)/11;
+    roc_struct_MN_h(:,id_h) = sum(roc_struct_MN,2)/11;
+
 end
-
-
-varNames = ["avg fbest", "avg gradf_norm","avg num of iters", "avg time of exec (sec)", "n failure", "avg roc"];
-rowNames = string(dimension');
-TMN = table(sum(fbest_struct_MN,2)/11, sum(gradf_struct_MN,2)/11 ,sum(iter_struct_MN,2)/11, sum(execution_time_MN,2)/11, sum(failure_struct_MN,2), sum(roc_struct_MN,2)/11,'VariableNames', varNames, 'RowNames', rowNames);
-format bank
-display(TMN)
 
